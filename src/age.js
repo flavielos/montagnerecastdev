bdc6e2627e888c7d334a636d71e5799c04f293ce
Flavie Loshouarn <Flavie Loshouarn@PVAL.com>
2017-07-20 12:21:32 +0200

    src

--- src/age.js
+++ src/age.js
@@ -1,12 +1,12 @@
-const age = (age1, age2) => {
-	return age1
+function age = (age1, age2) => {
+
 	var ageTemp;
 	if ( age1 > age2 ) {
 		ageTemp = age2;
 		age2 = age1;
 		age1 = ageTemp;
 	}
-	
+	return age1
 }
 
 module.exports = age
