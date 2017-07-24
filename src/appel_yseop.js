275137c28eeb9dd68448ff96abad92640994339c
Flavie Loshouarn <Flavie Loshouarn@PVAL.com>
2017-07-24 11:47:06 +0200

    src

--- src/appel_yseop.js
+++ src/appel_yseop.js
@@ -1,8 +1,15 @@
 var xhr = require('w3c-xmlhttprequest');
+var xtb = require ('./xmlToBlob');
+
 
 exports.appel = function(){
 	
+	
 	var req = new xhr.XMLHttpRequest();
+	console.log('blob');
+	var data = xtb.xmlToString();
+
+	console.log('blob');
 	req.onreadystatechange = function(){
 		if(req.readyState == 4 && (req.status == 200 || req.status == 0)){
 			console.log('onreadystatechange ok');
@@ -10,14 +17,12 @@ exports.appel = function(){
 			console.log('req.readyState : ' + req.readyState.toString() + ' & req.statusText : ' + req.statusText.toString());
 		};
 	};
-	req.open('POST', 'https://pval2.studio.yseop-hosting.com/yseop-manager/direct/Montagne/dialog.do?command=init-dialog', true);
-	//req.open('GET', 'https://stackoverflow.com/questions/4789613/ajax-500-internal-server-error', true);
+	req.open('POST', 'https://pval2.studio.yseop-hosting.com/yseop-manager/direct/Montagne/dialog.do?command=init-dialog&transformation=bot', true);
 	req.setRequestHeader('Content-Type', 'application/soap+xml; charset=UTF-8');
 	req.setRequestHeader('Authorization', 'Basic b3dpOmY1VFRuVmhBbUZmeHlsY2J1YlVNdDl0MlpYNURNTg==');
 	req.setRequestHeader('Accept', 'text/html');
-	req.send('./donnees_client.xml');
-	//req.send();
-
+	req.send(data);
+	
 	
 };
 
--- src/xmlToBlob.js
+++ src/xmlToBlob.js
@@ -1,16 +1,49 @@
 var xhr = require('w3c-xmlhttprequest');
-
-
-exports.xmlToBlob = function(xml){
-	var req = new XMLHttpRequest();
-	req.onreadystatechange = function(){
-		if(req.readyState == 4 && (req.status == 200 || req.status == 0)){
-			console.log('onreadystatechange ok');
-		}else{
-			console.log('req.readyState : ' + req.readyState.toString() + ' & req.statusText : ' + req.statusText.toString());
-		};
-	};
-	req.open('GET', './donnees_client.xml', true);
-	req.send(null);
-	//var xmlString = (new XMLSerializer()).serializeToString(xml);
-};
+var xml = require('simple-xml-dom')
+ 
+exports.xmlToString = function () { 
+	var source = '<?xml version="1.0" encoding="UTF-8"?>'+
+'<y:input xmlns:y="http://www.yseop.com/engine/3" xmlns:i18n="http://apache.org/cocoon/i18n/2.1" '+
+'xmlns:ft="http://apache.org/cocoon/forms/1.0#template" xmlns:fi="http://apache.org/cocoon/forms/1.0#instance" '+
+'xmlns:fd="http://apache.org/cocoon/forms/1.0#definition" xmlns:fb="http://apache.org/cocoon/forms/1.0#binding" >'+
+'	<y:action command="init-dialog" sub-command=""/>'+
+'	<y:data>'+
+'		<y:instance yid="theGeneralData">'+
+'			<traces>true</traces>'+
+'			<reloadedData>true</reloadedData>'+
+'			<saisieDeDonnees yid="SAISIE_1">'+
+'				<nomClient>Maxime</nomClient>'+
+'				<ageMax>55</ageMax>'+
+'				<ageMin>45</ageMin>'+
+'				<dureeDeLaRandonnee>4</dureeDeLaRandonnee>'+
+'				<dureeStricte yid="DUREE_PLUS"/>'+
+'				<isoleAccessible yid="ACCESSIBLE"/>'+
+'				<niveauEloignement yid="NV_ELOIGNEMENT_1"/>'+
+'				<niveauPhysique yid="NV_PHYSIQUE_2"/>'+
+'				<niveauRandonneur yid="NV_RANDONNEUR_2"/>'+
+'				<niveauEquipement yid="NV_EQUIPEMENT_1"/>'+
+'				<niveauDifficulte yid="NV_DIFFICULTE_1"/>'+
+'				<niveauBudget yid="NV_BUDGET_4"/>'+
+'				<niveauActivites yid="NV_ACTIVITES_4"/>'+
+'			</saisieDeDonnees>'+
+'			<reloadedData>false</reloadedData>'+
+'			<dialogId>853880</dialogId>'+
+'			<language yid="LANG_en"/>'+
+'		</y:instance>'+
+'	</y:data>'+
+'	<y:dialog-session>'+
+'		<y:dialog-id>R20170707070825-1</y:dialog-id>'+
+'	</y:dialog-session>'+
+'	<y:misc-in>'+
+'		<y:version dialog="2.19.0.S" manager="2.9.0"/>'+
+'		<y:language id="fr_FR"/>'+
+'		<y:language id="fr"/>'+
+'		<y:language id="en_US"/>'+
+'		<y:language id="en"/>'+
+'	</y:misc-in>'+
+'</y:input>';
+	xml.serialize(xml.parse(source))
+	console.log(xml.serialize(xml.parse(source)).toString());
+	return(xml.serialize(xml.parse(source)).toString());
+    //
+}   
